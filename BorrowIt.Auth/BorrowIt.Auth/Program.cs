9deb1eb561265060dbe4ab23cdb9364c157e1761
Jakub Święch <jakub.swiech@kotrak.com>
2020-12-05 12:17:41 +0100

    Changes in configuration

--- BorrowIt.Auth/BorrowIt.Auth/Program.cs
+++ BorrowIt.Auth/BorrowIt.Auth/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -9,20 +11,34 @@ namespace BorrowIt.Auth
     {
         public static void Main(string[] args)
         {
-            // ASP.NET Core 3.0+:
-            // The UseServiceProviderFactory call attaches the
-            // Autofac provider to the generic hosting mechanism.
-            var host = Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webHostBuilder => {
-                    webHostBuilder
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>();
-                })
-                .Build();
-
-            host.Run();
+            var failuresCount = 0;
+            while (failuresCount <= 10)
+            {
+                try
+                {
+                    var host = Host.CreateDefaultBuilder(args)
+                        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                        .ConfigureWebHostDefaults(webHostBuilder => {
+                            webHostBuilder
+                                .UseContentRoot(Directory.GetCurrentDirectory())
+                                .UseIISIntegration()
+                                .UseStartup<Startup>();
+                        })
+                        .Build();
+                    host.Run();
+                    return;
+                }
+                catch (Exception e)
+                {
+                    if (!e.Source.Contains("RabbitMQ"))
+                    {
+                        throw;
+                    }
+                    failuresCount++;
+                    File.WriteAllText($"startError{failuresCount}.txt",e.ToString());
+                    Thread.Sleep(30000);
+                }
+            }
         }
     }
 }
--- BorrowIt.Auth/BorrowIt.Auth/Startup.cs
+++ BorrowIt.Auth/BorrowIt.Auth/Startup.cs
@@ -131,6 +131,7 @@ namespace BorrowIt.Auth
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
             app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
